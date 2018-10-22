43266e17d5cd93b6be7c2a3cccd9e9d04dd4e129
ratexx <ratexx@gmail.com>
2018-10-22 23:43:10 +0700

    Add Item UI

--- God Morning/TodoItem.swift
+++ God Morning/TodoItem.swift
@@ -7,3 +7,17 @@
 //
 
 import Foundation
+class TodoItem{
+    var title: String
+    var isDone: Bool
+    
+    init(title: String, isDone: Bool = false){
+            self.title = title
+            self.isDone = isDone
+        
+        func toggleIsDone() {
+            self.isDone = !self.isDone
+        }
+    }
+    
+}
